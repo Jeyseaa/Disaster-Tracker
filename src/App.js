df94416401c6c5d8bd97ce80b5258164a5bea488
Jeyseaa <jsea.balatan@gmail.com>
2024-04-09 14:13:51 +0800

    Second Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Loginpage from './components/Loginpage';
 import AdminLogin from './components/AdminLogin';
 import Admindashboard from './components/Admindashboard';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Router>
       <div className="App">
@@ -13,7 +25,17 @@ function App() {
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/login" element={<Loginpage />} />
           <Route exact path="/admin-login" element={<AdminLogin />} />
-          <Route exact path="/admin-dashboard" element={<Admindashboard />} />
+          <Route
+            exact
+            path="/admin-dashboard"
+            element={
+              user && user.uid === 'Wruc9pH0VlMJYbTO4D8bpUhzn8L2' ? (
+                <Admindashboard />
+              ) : (
+                <Navigate to="/admin-login" />
+              )
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import "../styles/adminlogin.css";
 
 const StyledAdminLogin = styled.div`
@@ -17,45 +17,19 @@ const StyledAdminLogin = styled.div`
 const AdminLogin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsAuthenticated(true);
-      } else {
-        setIsAuthenticated(false);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/admin-dashboard');
-    }
-  }, [isAuthenticated, navigate]);
-
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
-    if (!validateEmail(email)) {
-      setError('Please enter a valid email address.');
-      return;
-    }
     try {
       const auth = getAuth();
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           const user = userCredential.user;
           if (user.uid === 'Wruc9pH0VlMJYbTO4D8bpUhzn8L2') {
-            setIsAuthenticated(true);
-            navigate('/admin-dashboard');
+            navigate('/admin-dashboard'); // Navigate to Admin Dashboard if the user is authorized
           } else {
-            setIsAuthenticated(false);
             setError('You are not authorized to access the admin dashboard.');
           }
         })
@@ -66,58 +40,49 @@ const AdminLogin = () => {
       setError('An error occurred. Please try again.');
     }
   };
-  
-  
-  const validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-  
 
   return (
     <StyledAdminLogin>
-      {isAuthenticated ? null : (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-              <div className="account-wall">
-                <img className="profile-img" src="https://scontent.fmnl30-1.fna.fbcdn.net/v/t1.15752-9/434199910_1077982733462085_7343354962094345715_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Qiz917JKKNYAb4P32Ly&_nc_ht=scontent.fmnl30-1.fna&oh=03_AdXgSn-eAW3Xv7xRf-fOIyUw9qReugZdkTs6OjCzK3pr0Q&oe=6635C4E9" alt="Profile" />
-                <form className="form-signin" onSubmit={handleLoginSubmit}>
-                  <input
-                    type="text"
-                    className="form-control custom-input"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                  <input
-                    type="password"
-                    className="form-control custom-input"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                  <div className="form-group row">
-                    <div className="col-lg-6 col-md-6 col-sm-6">
-                      <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="remember-me" />
-                        <label className="form-check-label" htmlFor="remember-me">Remember Me</label>
-                      </div>
-                    </div>
-                    <div className="col-lg-6 col-md-6 col-sm-6 text-right">
-                      <a href="#forgot" className="forgot-link">Forgot Password</a>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+            <div className="account-wall">
+              <img className="profile-img" src="https://scontent.fmnl30-1.fna.fbcdn.net/v/t1.15752-9/434199910_1077982733462085_7343354962094345715_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Qiz917JKKNYAb4P32Ly&_nc_ht=scontent.fmnl30-1.fna&oh=03_AdXgSn-eAW3Xv7xRf-fOIyUw9qReugZdkTs6OjCzK3pr0Q&oe=6635C4E9" alt="Profile" />
+              <form className="form-signin" onSubmit={handleLoginSubmit}>
+                <input
+                  type="text"
+                  className="form-control custom-input"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <input
+                  type="password"
+                  className="form-control custom-input"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <div className="form-group row">
+                  <div className="col-lg-6 col-md-6 col-sm-6">
+                    <div className="form-check">
+                      <input type="checkbox" className="form-check-input" id="remember-me" />
+                      <label className="form-check-label" htmlFor="remember-me">Remember Me</label>
                     </div>
                   </div>
-                  {error && <div className="error-message">{error}</div>}
-                  <button type="submit" className="btn btn-lg btn-saffron btn-block">Login</button>
-                </form>
-              </div>
+                  <div className="col-lg-6 col-md-6 col-sm-6 text-right">
+                    <a href="#forgot" className="forgot-link">Forgot Password</a>
+                  </div>
+                </div>
+                {error && <div className="error-message">{error}</div>}
+                <button type="submit" className="btn btn-lg btn-saffron btn-block">Login</button>
+              </form>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </StyledAdminLogin>
   );
 }
